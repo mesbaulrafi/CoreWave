26fedadc8ed42e7b98a83a6ebbecf72433bcb1cf
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-23 13:10:40 +0600

    add services Part

---
+++ src/components/layouts/Services.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Containar from '../Containar'
+
+const Services = () => {
+  return (
+    <>
+    <div className="">
+        <Containar>
+            <div className="flex">
+                <div className=""></div>
+                <div className=""></div>
+            </div>
+        </Containar>
+    </div>
+    </>
+  )
+}
+
+export default Services
