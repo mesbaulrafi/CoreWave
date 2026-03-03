872ed48fcea3f8f998c2c0e1aa5913020ff85390
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-03-03 22:17:15 +0600

    edit service card

--- src/components/layouts/ServicesCard.jsx
+++ src/components/layouts/ServicesCard.jsx
@@ -5,10 +5,11 @@ import UiuxLogo from '../../assets/logo/UiuxLogo'
 const ServicesCard = () => {
   return (
    <>
-   <div className="">
-    <div className="">
+   <div className="bg-white w-[312px] rounded-[10px]">
+    <div className="pt-10.5 pl-10.5 pb-15 ">
         <WebLogo/>
-        <h4 className=''>Web Development</h4>
+        <h4 className='font-lato font-semibold text-[20px] leading-7.5 inline-block py-3.5'>Web Development</h4>
+        <p className='font-lato  text-base leading-6 text-[#7B7B7B] w-[80%]'>Web development is the art of creating engaging and visually appealing websites </p>
     </div>
    </div>
    </>
