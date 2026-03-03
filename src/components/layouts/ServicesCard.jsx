506a38adecddb63f3a8995f643aa5861162647da
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-03-03 13:02:55 +0600

    serviceLogo Add

--- src/components/layouts/ServicesCard.jsx
+++ src/components/layouts/ServicesCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import WebLogo from '../../assets/logo/WebLogo'
 
 const ServicesCard = () => {
   return (
    <>
    <div className="">
     <div className="">
-        
+        <WebLogo/>
     </div>
    </div>
    </>
