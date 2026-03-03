5feb6eb70d7fd4aa62657f161784e09f73579c42
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-03-04 00:21:34 +0600

    service cadr dinamic

--- src/components/layouts/ServicesCard.jsx
+++ src/components/layouts/ServicesCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import WebLogo from '../../assets/logo/WebLogo'
 
 
-const ServicesCard = () => {
+const ServicesCard = ({cardLogo,cardTitle,cardText,}) => {
   return (
    <>
    <div className="bg-white w-[312px] rounded-[10px]">
     <div className="pt-10.5 pl-10.5 pb-15 ">
-        <WebLogo/>
-        <h4 className='font-lato font-semibold text-[20px] leading-7.5 inline-block py-3.5'>Web Development</h4>
-        <p className='font-lato  text-base leading-6 text-[#7B7B7B] w-[80%]'>Web development is the art of creating engaging and visually appealing websites </p>
+        <i className=''>{cardLogo}</i>
+        <h4 className='font-lato font-semibold text-[20px] leading-7.5 inline-block py-3.5'>{cardTitle}</h4>
+        <p className='font-lato  text-base leading-6 text-[#7B7B7B] w-[80%]'>{cardText} </p>
     </div>
    </div>
    </>
