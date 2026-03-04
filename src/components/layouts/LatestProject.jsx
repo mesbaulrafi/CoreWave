a16a441256ee251e1af3e09a3374d143ec46ef78
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-03-05 01:09:27 +0600

    add our latest project part

---
+++ src/components/layouts/LatestProject.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LatestProject = () => {
+  return (
+    <>
+    <div className="">
+        <div className=""></div>
+    </div>
+    </>
+  )
+}
+
+export default LatestProject
